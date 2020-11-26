60d772b92f30656af33fd7c4617850284efbe547
Mauricio Bucca <mb2322@cornell.edu>
2020-11-26 20:59:52 -0300

    Update

--- slides/class_13/class_13.R
+++ slides/class_13/class_13.R
@@ -41,23 +41,24 @@ for (dv in c("total_cases_per_million", "total_deaths_per_million")) {
 
 # Purrr
 
-
 covid_data <- covid_data %>% 
   dplyr::select(continent,location,total_cases_per_million, 
                 total_deaths_per_million, population_density,median_age, gdp_per_capita) %>%
   pivot_longer(cols=c(total_cases_per_million,total_deaths_per_million), 
                names_to="dv", values_to="y") %>% 
-  pivot_longer(c(population_density,median_age,gdp_per_capita), 
+  pivot_longer(cols=c(population_density,median_age,gdp_per_capita), 
                names_to="iv", values_to="x")  
-
-covid_data
 
 
 fit_lm <- covid_data %>% group_by(continent,dv,iv) %>% nest() %>%
   mutate(fit = map(data, ~ lm(y ~ x, data = .x))) %>% 
   mutate(coefs = map(fit, tidy)) %>% 
-  dplyr::select(continent,dv,iv,coefs) %>% 
-  unnest()
+  unnest(cols = c(continent,dv,iv,coefs)) %>% select(-c(data,fit))
+
+
+# Nota: como acceder a una base de datos en particular
+#a <- covid_data %>% group_by(continent,dv,iv) %>% nest()
+#a$data[[which(a$dv=="total_cases_per_million" & a$iv=="median_age" & a$continent=="Europe")]]
 
 
 plot <- fit_lm %>% filter(term=="x") %>%  
