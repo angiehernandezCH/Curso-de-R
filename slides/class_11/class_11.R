66b83c350b1a21eb33809b08b9e6df6179a37b11
Mauricio Bucca <mb2322@cornell.edu>
2021-10-19 20:00:55 -0300

    Update

--- slides/class_11/class_11.R
+++ slides/class_11/class_11.R
@@ -1,70 +1,21 @@
-# Limpiar pantalla y remover objetos existentes
-cat("\014") 
-rm(list = ls())
-
-## Bases de datos ordenadas ("tidy") 
-
+library("readr")
 library("tidyverse")
-library("ineq")
-library("ggsci")
-library("viridis")
+library("ggplot2")
 
-setwd(
-  "~/Library/Mobile Documents/com~apple~CloudDocs/Teaching/ISUC/2020_2_data_analysis_r/repo/slides/class_5/")
+x <- url("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv")
+data_covid <- read_csv(x)
 
-# leer archivo csv
-data_casen_csv <- read_csv("sample_casen2017.csv")
-data_casen_csv %>% glimpse()
+data_covid %>% glimpse()
 
-data_ineq <- data_casen_csv %>% 
-  group_by(comuna,educ,sexo) %>% 
-  summarise(gini_ytot=Gini(ytotcor,na.rm = T), gini_yaut=Gini(yautcor,na.rm = T), media_yaut=mean(yautcor,na.rm = T)) %>%
-  mutate(ratio=gini_ytot/gini_yaut) %>%
-  mutate(educ = if_else(educ==99,NA_real_,educ))
-
-# Construir gráfico en layers
-
-# paso 1:
-
-data_ineq %>% ggplot()
-
-#o
-
-ggplot(data=data_ineq) 
+data_covid <- data_covid %>% 
+  pivot_longer(cols = -c(Region,`Codigo region`,Comuna,`Codigo comuna`,Poblacion), 
+               names_to = "Fecha", values_to = "Casos_diarios") %>%
+  mutate(Fecha = as.Date(Fecha, format = "%Y-%m-%d"))
 
 
-# paso 2
-
-# color continuo
-data_ineq %>% ggplot(aes(x=media_yaut, y=gini_yaut)) + geom_point()
-                     
-g <- data_ineq %>% ggplot(aes(x=log(media_yaut), y=gini_yaut, colour=educ)) +
-  geom_point(size=2, alpha=0.4) +
-  geom_hline(yintercept = 0.5) +
-  geom_vline(xintercept = 10) +
-  ylim(0,0.7) +
-  theme_bw() +
-  facet_grid( sexo ~ .) +
-  labs(x="Log median ingreso autónomo indiv.",  y="Gini Ingreso autonomo indiv.", color = "Educación") +
-  scale_color_viridis(option="magma") 
-
-g
-
-data_ineq %>% ggplot(aes(x=log(media_yaut), y=gini_yaut, colour=educ)) +
-  geom_point(size=2, alpha=0.4) +
-  geom_hline(yintercept = 0.5) +
-  geom_vline(xintercept = 10) +
-  ylim(0,0.7) +
-  theme_bw() +
-  facet_grid( . ~ sexo) +
-  labs(x="Log median ingreso autónomo indiv.",  y="Gini Ingreso autonomo indiv.", color = "Educación") +
-  scale_color_viridis(option="magma") 
-
-
-# RETOMAR: jitter + transparencia
-
-data_casen_csv %>% ggplot(aes(x=esc, y=yautcor, colour=sexo)) +
-  geom_point(size=2, alpha=0.4) +
-  theme_bw() +
-  scale_color_viridis(option="magma") 
-
+data_covid %>% 
+              ggplot(aes(x=Fecha, y=Casos_diarios, group=Comuna)) +
+               geom_line(alpha=0.3) +
+               theme(legend.position = "none") +
+               labs(x="Fecha", y="Número de Casos Diarios", title = "Evolución casos Covid-19") +
+               theme_bw()
