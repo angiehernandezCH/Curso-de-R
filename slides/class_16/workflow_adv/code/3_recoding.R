63809879e09152c14970cc43a7ab88cb1be2637e
Mauricio Bucca <mb2322@cornell.edu>
2021-11-10 11:16:12 -0300

    Update

--- slides/class_16/workflow_adv/code/3_recoding.R
+++ slides/class_16/workflow_adv/code/3_recoding.R
@@ -1,34 +1,13 @@
 
-# Seleccionar set de datos de interés
-
-essdata_sub <-  essdata %>% select(idno,cntry,gndr,yrbrn,eisced,hinctnta)
-
-
-
-# filtra filas que cumplen condición lógica 
-
-
-essdata_sub <- essdata_sub %>% filter(cntry!="AT", yrbrn>1910)
-
-
 # crea nueva variable
-
-essdata_sub <- essdata_sub %>% mutate(age = 2019 - yrbrn)
-
-
-
+essdata <- essdata %>% mutate(age = 2019 - yrbrn)
 
 # recodifica variable existente
+essdata <- essdata %>% mutate(eisced = na_if(eisced, 55)) # eisced==55 es NA
 
+essdata <- essdata %>% mutate(generation = if_else(yrbrn > 2000, "Millenial", "Viejo"))
 
-essdata_sub <- essdata_sub %>% mutate(eisced = na_if(eisced, 55)) # eisced==55 es NA
-
-
-
-essdata_sub <- essdata_sub %>% mutate(generation = if_else(yrbrn > 2000, "Millenial", "Viejo"))
-
-
-essdata_sub <- essdata_sub %>%
+essdata <- essdata %>%
  mutate(gndr_string = case_when(
 	gndr == 1 ~ "Male",
 	gndr == 2 ~ "Female"
