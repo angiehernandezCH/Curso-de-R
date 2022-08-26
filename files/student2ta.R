e4d90479d5fcf0261f5f812b53a5ef7722b5ad16
Mauricio Bucca <mb2322@cornell.edu>
2022-08-26 11:01:35 -0400

    Update

--- files/student2ta.R
+++ files/student2ta.R
@@ -1,18 +1,18 @@
+library("tidyverse")
 library("readxl")
 library("knitr")
 
-set.seed(6158)
+set.seed(615)
 
-ayudantes <- tibble(Ayudante=c("Matias","Martín ","Sebastian"), Grupo=1:3)
+ayudantes <- tibble(Ayudante=c("Matias","Martín","Sebastian"), Grupo=1:3)
 
-path <- "Library/Mobile Documents/com~apple~CloudDocs/Teaching/ISUC/2022_2_data_analysis_r/repo/files/listado_estudiantes.xls"
+path <- "/Users/Mauricio/Library/Mobile Documents/com~apple~CloudDocs/Teaching/ISUC/2022_2_data_analysis_r/repo/files/listado_estudiantes.xlsx"
 
-listado_estudiantes <- read_excel(path) %>%
+listado_estudiantes <- read_excel(path) %>% dplyr::select(Nombres) %>% rename(Estudiante = Nombres) %>%
   rowwise() %>%
-  mutate(Estudiante = paste0(Nombre," ",Apellido) ) %>%
   mutate(Grupo = sample(1:3,1)) %>%
   left_join(ayudantes,by="Grupo") %>%
   arrange(Grupo) %>%
-  select(Grupo,Ayudante,Estudiante)
+  dplyr::select(Grupo,Ayudante,Estudiante)
 
-
+kable(listado_estudiantes)
