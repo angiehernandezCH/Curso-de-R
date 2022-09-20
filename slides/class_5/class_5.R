a5efdd76fd9c1af44f7133ef3841f51ba791436a
Mauricio Bucca <mb2322@cornell.edu>
2022-09-20 10:15:34 -0300

    Update

--- slides/class_5/class_5.R
+++ slides/class_5/class_5.R
@@ -20,7 +20,8 @@ setwd(
 )
 
 
-read_dta("sample_casen2017.dta")
+read_dta("sample_casen2017.dta") # problema. Necesitamos especificar algunas opciones (ver abajo)
+
 # leer archivo dta 
 data_casen_dta <- read_dta("sample_casen2017.dta", encoding = "latin1", .name_repair = "minimal")
 
@@ -78,77 +79,12 @@ data_casen_csv %>%
   filter(sexo==2 | region==13) %>% 
   select(sexo, region) %>% head()
 
-# Creación de nuevas variables
-
-data_casen_csv <- data_casen_csv %>% 
-  mutate(anno = 2017) %>% 
-  mutate(ln_ytotcor_mm = log((ytotcor + 1)/1000)) %>%
-  
-  
-  
-  data_casen_csv %>% select(sexo,edad,ytotcor,anno,ln_ytotcor_mm) %>%
-  select(!ln_ytotcor_mm)
 
 
+########################### Ejercicio ################################
 
-# mutate, if_else: creación de datos
+#Selecciona los individuos con un ingreso total (ytotcor) de 10 millones y cuenta cuantos hay en cada región
 
-data_casen_csv %>% select(sexo,edad,ytotcor,anno,ln_ytotcor_mm) %>%
-  mutate(sexo2 = if_else(sexo==1,1,0)) %>% select(sexo,sexo2) %>%
-  with(table(sexo,sexo2))
-
-
-data_casen_csv %>% select(sexo,edad,ytotcor) %>% 
-  mutate(sexo = if_else(sexo==1,sexo,0))
-
-data_casen_csv %>% select(sexo,edad,ytotcor) %>% 
-  mutate(sexo = if_else(sexo==1,edad,0))
-
-
-## mutate, case_when: creación de datos
-
-
-data_casen_csv %>% select(sexo,edad,ytotcor) %>% 
-  mutate(edad_cat = case_when(edad <= 18 ~ 1, 
-                              edad >18 & edad<=65 ~ 2, 
-                              edad > 65  ~ 3) 
-  )
-
-
-data_casen_csv %>% select(sexo,edad,ytotcor) %>% 
-  mutate(edad_cat = case_when(edad <= 18 ~ 1,
-                              edad >18 & edad<=65 ~ 2, 
-                              edad > 65  ~ 3, 
-                              TRUE ~ edad)
-  )
-
-
-
-## group_by: operaciones agrupadas.
-
-cuadrado <- function(x) {x^2}
-
-data_casen_csv %>% 
-  group_by(region) %>% 
-  mutate(n_region = n(), mean_ytotcor_region = mean(ytotcor, na.rm = T), cuadrado_edad = cuadrado(edad)) %>% 
-  ungroup() %>% 
-  select(region,sexo,edad, ytotcor,n_region, mean_ytotcor_region,cuadrado_edad ) 
-
-
-data_casen_csv <- data_casen_csv %>% 
-  mutate(edad_cat = case_when(edad <= 18 ~ 1,
-                              edad >18 & edad<=65 ~ 2, 
-                              edad > 65  ~ 3, 
-                              TRUE ~ edad))   %>% 
-  group_by(region, sexo, edad_cat) %>% 
-  mutate(n_region = n(), mean_ytotcor_region = mean(ytotcor, na.rm = T)) %>%
-  ungroup() %>%
-  select(!edad_cat)
-
-
-# sampling by group
-
-data_casen_csv %>% group_by(sexo) %>% sample_n(size = 4, replace = T)
-
+data_casen_csv %>% filter(ytotcor>=10000000) %>% select(region)
 
 
